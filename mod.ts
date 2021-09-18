289f1a228d472dbab1c7827f04bb10b0e029637b
jia wei <jw397@126.com>
2021-09-18 18:08:48 +0800

    feat: version

--- mod.ts
+++ mod.ts
@@ -3,3 +3,5 @@ export * from "./src/aes/mod.ts";
 export * from "./src/hmac/mod.ts";
 export * from "./src/otp/totp.ts";
 export * from "./src/utility/encode.ts";
+
+export const version = "1.14.1";
